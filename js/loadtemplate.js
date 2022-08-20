45cd942d0cfdd70f45eb4abc2094698fd6857d34
WT-MM <wesley.maa@gmail.com>
2022-08-20 06:11:14 -0700

    home page

--- js/loadtemplate.js
+++ js/loadtemplate.js
@@ -5,3 +5,12 @@ $(function () {
       $(this).load(file)
     })
   })
+
+
+function load(addr, newPage){
+  if(newPage){
+    window.open('https://' + linaddrk, '_blank').focus();
+  }else{
+    window.location=addr
+  }
+}
