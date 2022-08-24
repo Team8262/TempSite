8509708bb68e78fbeadce989ade5c8bdc017dd9c
WT-MM <wesley.maa@gmail.com>
2022-08-23 20:43:18 -0700

    implement media

--- js/loadtemplate.js
+++ js/loadtemplate.js
@@ -1,9 +1,13 @@
+var loaded = false
+
 $(function () {
     var includes = $('[data-include]')
     $.each(includes, function () {
       var file = '../templates/' + $(this).data('include') + '.html'
       $(this).load(file)
     })
+    loaded=true
+    console.log("Templates loaded")
   })
 
 
